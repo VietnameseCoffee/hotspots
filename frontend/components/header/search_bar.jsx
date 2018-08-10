f5e0f3358ca6079ae3751a1ca5f1916d1f705151
Kevin Ou <kevin.ou705@gmail.com>
2018-08-09 17:31:05 -0700

    Add form divs in searchbar

--- frontend/components/header/search_bar.jsx
+++ frontend/components/header/search_bar.jsx
@@ -4,14 +4,36 @@ class SearchBar extends React.Component {
 
   constructor (props) {
     super(props);
-
+    this.state = {
+      business: "",
+      location: "",
+    };
   }
+
+  // update (value) {
+  //   return (e) => this.setState({[value]: e.target.value});
+  // }
+  //
+  // handleSubmit (e) {
+  //   e.preventDefault();
+  //   this.props.action(this.state).then(() => this.props.history.push('/'));
+  // }
 
   render ()  {
 
     return (
-    <div>Search Bar
-
+    <div className="search_bar">Search Bar
+      <form onSubmit={() => console.log("searchin..")}>
+        <span className="search-business">
+          <label id="business"></label>
+          <input for="business"></input>
+        </span>
+        <span className="search-location">
+          <label id="location"></label>
+          <input for="location"></input>
+        </span>
+        <button>SUBMIT YO</button>
+      </form>
     </div>
     );
   }
