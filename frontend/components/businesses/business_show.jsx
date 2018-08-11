149c3210e8d338f8a62e89661048705824186db3
Kevin Ou <kevin.ou705@gmail.com>
2018-08-11 13:13:41 -0700

    Create business show to fetch data

--- frontend/components/businesses/business_show.jsx
+++ frontend/components/businesses/business_show.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 class BusinessShow extends React.Component {
 
   constructor (props) {
     super(props);
-
+    this.state = {};
   }
 
   componentDidMount () {
-
+    this.props.requestBusiness(this.props.match.params.businessId);
   }
 
   render () {
+    // console.log(this.props.match.params.businessId)
     return (
       <div className="business-show">Business Show!</div>
     );
