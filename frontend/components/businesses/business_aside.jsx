812bc5536d902b26a1fe95a0e980a636da3312ab
Kevin Ou <kevin.ou705@gmail.com>
2018-08-12 15:22:42 -0700

    Add hours to show page

--- frontend/components/businesses/business_aside.jsx
+++ frontend/components/businesses/business_aside.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const BusinessAside = (props) => {
+const BusinessAside = ({ hours }) => {
+  let hoursList = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun",];
+
+  hoursList = hoursList.map((el, i) => <li><span>{el}</span><span>{hours[i]}</span></li>)
 
   return (
     <div className="biz-aside-container">Aside Hur
 
       <div className="aside-info">
-        Hours
+        <h3>Hours</h3>
+        <ul>
+          {hoursList}
+        </ul>
       </div>
 
       <div className="aside-info">
