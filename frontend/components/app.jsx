c0ca614eeb796e698da7ef0842581a9d26ec29d5
Kevin Ou <kevin.ou705@gmail.com>
2018-08-14 15:34:50 -0700

    Create reviewform component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,6 +8,7 @@ import Header from './header/header.jsx';
 import SignupHeader from './header/signup_header.jsx';
 import SplashHeader from './splash/splash_header';
 import BusinessShowContainer from './businesses/business_show_container';
+import ReviewForm from './reviews/review_form'
 
 
 const App = () => {
@@ -26,8 +27,8 @@ const App = () => {
         <AuthRoute exact path="/login" component={ LoginFormContainer }/>
         <AuthRoute exact path="/signup" component={ SignupFormContainer }/>
         <Route path={`/businesses/:businessId`} component={BusinessShowContainer} />
+        <Route path={`/writeareview/:businessId`} component={ReviewForm} />
       </div>
-
       links to show page----
       <Link to="/businesses/1">Good Mong Kok Bakery</Link>-----
       <Link to="/businesses/2">Wildflower Cafe</Link>-----
--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-class ReviewForm extends React.component {
+class ReviewForm extends React.Component {
 
   constructor (props) {
-    
+    super(props);
+
+    this.state = { text: '', ratings: 0 };
+  }
+
+  update (value) {
+    return (e) => this.setState({[value]: e.target.value});
+  }
+
+  // handleSubmit (e) {
+  //   e.preventDefault();
+  //   this.props.action(this.state);
+  // }
+
+
+  render () {
+    return (
+      <div className="">
+      <h2>BUSINESS NAME LINK</h2>
+      <form >
+
+        <div className="stars">
+          <input
+            className="input-field"
+            placeholder="stars"
+            type='text'
+            onChange={this.update('stars')}
+            value={this.state.stars}></input>
+        </div>
+
+        <div>
+          <textarea
+            placeholder="Write your review inside here!"
+            type='text'
+            onChange={this.update('text')}
+            value={this.state.text}></textarea>
+        </div>
+
+        <div>
+          <button className='button'>Post Review</button>
+        </div>
+
+      </form>
+    </div>
+    );
   }
 }
 
