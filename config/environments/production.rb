7bba83cc090518c53fc0150502ca2289692cdac5
Kevin Ou <kevin.ou705@gmail.com>
2018-08-15 11:28:08 -0700

    Add activestorage aws stuff to productionrb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.active_storage.service = :amazon_dev
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
