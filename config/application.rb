2f77998ef010086c21d4717ef37045af76114b93
Kevin Ou <kevin.ou705@gmail.com>
2018-08-14 17:23:03 -0700

    Fix aws seahorse issue

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Hotspots
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    Aws.use_bundled_cert!
   end
 end
