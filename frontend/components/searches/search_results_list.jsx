15df58efdc3cffbf6f17c56195363f8fbc63a469
= <kevin.ou705@gmail.com>
2018-08-17 11:31:18 -0700

    Convert serch list to a class component

--- frontend/components/searches/search_results_list.jsx
+++ frontend/components/searches/search_results_list.jsx
@@ -2,25 +2,35 @@ import React from 'react';
 
 import BusinessIndexItem from '../businesses/business_index_item';
 
-const SearchResultList = ({businesses, requestBusiness}) => {
+class SearchResultList extends React.Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+
+  render () {
+    let businesses = this.props.businesses;
+    let requestBusiness = this.props.requestBusiness;
   return (
-    <div className="search-main">
-      <h2>Search Results</h2>
-      <div className="search-list">
-        <ul>
-        {businesses.map ((biz, i) => {
-          return (
-            <BusinessIndexItem
-            key={biz.id}
-            biz={biz}
-            index={i + 1}
-            requestAction={requestBusiness}
-            />)
-          })}
-          </ul>
+      <div className="search-main">
+        <h2>Search Results</h2>
+        <div className="search-list">
+          <ul>
+          {businesses.map ((biz, i) => {
+            return (
+              <BusinessIndexItem
+              key={biz.id}
+              biz={biz}
+              index={i + 1}
+              requestAction={requestBusiness}
+              />)
+            })}
+            </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default SearchResultList;
