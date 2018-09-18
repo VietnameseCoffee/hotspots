dffc9ac3cefeccad8200f0599e205a10514c7466
Nate <nathanharris.sf@gmail.com>
2018-09-18 10:24:04 -0700

    Integrate classnames npm package

--- frontend/components/businesses/business_images.jsx
+++ frontend/components/businesses/business_images.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
 const BusinessImages = ({ photoUrls }) => {
 
   const photos = photoUrls.slice(0, 3).map((url, i) => {
+    let classes = classNames({
+      [`biz-show-img-${i}`]: true,
+      'biz-img-default': i === 1
+    })
+
     return (
-      <div key ={url.photoUrl} className={`biz-show-img-${i}`}>
+      <div key ={url.photoUrl} className={classes}>
       <img src={url.photoUrl}></img>
       </div>
     )}
