31219e1265fad244fa3bb4f95eb0b95a31d4354a
Nate <nathanharris.sf@gmail.com>
2018-09-18 10:45:41 -0700

    Convert images to a class, prepare for future event listeners

--- frontend/components/businesses/business_images.jsx
+++ frontend/components/businesses/business_images.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
-const BusinessImages = ({ photoUrls }) => {
+class BusinessImages extends React.Component {
 
-  const photos = photoUrls.slice(0, 3).map((url, i) => {
-    let classes = classNames({
-      [`biz-show-img-${i}`]: true,
-      'biz-img-default': i === 1
-    })
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    // ReactDOM.findDOMNode(this).addEventListener('onMouseOut')
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render () {
+    const photos = this.props.photoUrls.slice(0, 3).map((url, i) => {
+      let classes = classNames({
+        [`biz-show-img-${i}`]: true,
+        'biz-img-default': i === 1
+      });
+      return (
+        <div key ={url.photoUrl} className={classes}>
+          <img src={url.photoUrl}></img>
+        </div>
+      )}
+    );
 
     return (
-      <div key ={url.photoUrl} className={classes}>
-      <img src={url.photoUrl}></img>
+      <div className="biz-images-container">
+        <div className="biz-images">
+          {photos}
+        </div>
       </div>
-    )}
-  );
-
-  return (
-    <div className="biz-images-container">
-      <div className="biz-images">
-        {photos}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default BusinessImages;
