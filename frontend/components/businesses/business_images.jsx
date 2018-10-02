5452311a7ab2e98157873e692575d8de36d6a2e5
Kevin Ou <kevin.ou705@gmail.com>
2018-10-02 00:02:04 -0700

    Add image grower and shrinker

--- frontend/components/businesses/business_images.jsx
+++ frontend/components/businesses/business_images.jsx
@@ -6,14 +6,45 @@ class BusinessImages extends React.Component {
 
   constructor(props) {
     super(props)
+
+    this.growImage = this.growImage.bind(this);
+    this.shrinkImage = this.shrinkImage.bind(this);
+  }
+
+  growImage() {
+    middle.classList.add('biz-img-default')
+  }
+
+  shrinkImage() {
+    middle.classList.remove('biz-img-default')
   }
 
   componentDidMount() {
-    // ReactDOM.findDOMNode(this).addEventListener('onMouseOut')
+    const imageContainer = document.querySelector('.biz-images')
+    const middle = document.querySelector('.biz-show-img-1')
+
+    if (middle) {
+      imageContainer.addEventListener('mouseleave', () => {
+        middle.classList.add('biz-img-default')
+      })
+      imageContainer.addEventListener('mouseenter', () => {
+        middle.classList.remove('biz-img-default')
+      })
+    }
   }
 
   componentWillUnmount() {
+    const imageContainer = document.querySelector('.biz-images')
+    const middle = document.querySelector('.biz-show-img-1')
 
+    if (middle) {
+      imageContainer.removeEventListener('mouseleave', () => {
+        middle.classList.add('biz-img-default')
+      })
+      imageContainer.removeEventListener('mouseenter', () => {
+        middle.classList.remove('biz-img-default')
+      })
+    }
   }
 
   render () {
